4992694edd86cbbc9cdc03740bd6e4186ffeb8b4
Santino-Gonzalez <96146946+Santino-Gonzalez@users.noreply.github.com>
2024-11-17 21:18:01 -0300

    Mezclar Colores
    Se agrega la función que mezcla los colores para iniciar el juego.

--- main.js
+++ main.js
@@ -2,9 +2,43 @@
 
 //EMPEZAR JUEGO
 
+document.querySelector("#empezar").onclick = function () {
+
+}
+
 //RESETEAR JUEGO
 
+document.querySelector("#resetear").onclick = function () {
+
+}
+
 //FUNCION MEZCLAR COLORES
+
+function mezclarColores() {
+    const colores = document.querySelectorAll(".color");
+    const tablero = document.querySelector("#tablero");
+    let arrayColores = [];
+
+    colores.forEach(color => {
+        arrayColores.push(color);
+    })
+    
+    arrayColores.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < colores.length; i++) {
+        tablero.removeChild(colores[i]);
+    }
+    for (let i = 0; i < arrayColores.length; i++) {
+        tablero.appendChild(arrayColores[i]);
+    }
+
+    arrayColores.forEach(color => {
+        color.style.background = color.id;
+        setTimeout(function () {
+            color.style.background = "white";
+        }, 750);
+    });
+}
 
 //FUNCION TURNO DEL JUGADOR
 
